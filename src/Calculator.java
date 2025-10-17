7d1e6d457ed4cba034b1541923801462b1e673bb
aram0117 <dkfka568355@gmail.com>
2025-10-17 16:47:11 +0900

    1단계 2차 수정코드

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,8 +1,25 @@
 import java.util.Scanner;
 
-public class Calculator {
-    // 사용자로부터 입력받은 두 정수 사칙연산 실행
-    public void calculate {
+public class Calculator extends App {
+
+    // 1.입력 기능
+    @Override
+    public void input () {
+
+        Scanner sc = new Scanner(System.in);
+
+        // 부호를 입력
+        System.out.println("[계산기] + = * /\n연산할 부호를 입력하세요");
+        char sign = sc.next().charAt(0);
+        // 첫번째 정수를 입력
+        System.out.println("첫번째 정수를 입력하세요");
+        int a = sc.nextInt();
+
+        // 두번째 정수를 입력
+        System.out.println("두번째 정수를 입력하세요");
+        int b = sc.nextInt();
+
+        // 2.연산 기능
         // 부호로 연산 할때 정수형으로 바꿔서 정상작동
 
         // 더하기가 입력되면 실행
@@ -22,29 +39,24 @@ public class Calculator {
         }
         //나누기가 입력되면 실행
         else if (sign == '/') {
-            int div = a / b;
+            int div = (int) a / b;
             System.out.println("정답 = " + div);
         }
     }
 
-    public void restart() {
-        Scanner scanner = new Scanner(System.in);
 
+    // 3.종료 기능
+    public void exit() {
+        Scanner scanner = new Scanner(System.in);
         //게산이 끝난 후 재실행 여부입력
-        System.out.println("restart : [restart를 입력하면 계산기가 재실행됩니다], exit : [restart외에 모든 문자 입력시 종료됩니다]+");
+        System.out.println("restart : [restart를 입력하면 계산기가 재실행됩니다], exit : [restart외에 모든 문자 입력시 종료됩니다]");
         String input = scanner.next();
 
         // 계산기 재실행
         if (input.equalsIgnoreCase("restart")) {
-            App();
-            restart();
+            input();
+            exit();
         }
     }
 }
-
-
-
-
-
-
 
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
-
         // 정수 입력란에 다른 자료형 입력시 계산기종료
         try {
-            calc.App();
-            calc.restart();
+            calc.input();
+            calc.exit();
         } catch (Exception e) {
             System.out.println("잘못된 접근입니다 계산기를 종료합니다");
         }
     }
 }
+
 
 
